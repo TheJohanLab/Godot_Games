101e831639ef16e3021c940d3a0ab724e3bcb0d6
Falry <maxime.castaing@telecomnancy.net>
2025-02-03 23:07:00 +0100

    end + collision wall

--- saucecurryryn/scenes/game.gd
+++ saucecurryryn/scenes/game.gd
@@ -12,3 +12,14 @@ func _ready() -> void:
 func reset_player():
 	player.velocity = Vector2.ZERO
 	player.global_position = start.get_spawn_pos()
+	
+
+
+func _on_end_body_entered(body: Node2D) -> void:
+	if body == player:
+		reset_player()
+
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if body.is_in_group("walls"):
+		reset_player()
--- saucecurryryn/scripts/player.gd
+++ saucecurryryn/scripts/player.gd
@@ -1,11 +1,17 @@
 extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const SPEED = 300.0
+const ROTATION_SPEED = 1.0
+const JUMP_VELOCITY = -400.0
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+ 
+func _physics_process(delta: float) -> void:
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+		
+	var direction = Input.get_vector("left","right","up", "down")	
+	velocity = direction * SPEED
+	rotation += ROTATION_SPEED * delta
+	move_and_slide()
